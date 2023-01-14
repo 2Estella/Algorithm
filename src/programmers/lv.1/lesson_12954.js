cd1fa1dc5510a561f813bc844d61d1dc57ec939c
2Estella <krsd0501@naver.com>
2023-01-14 17:11:17 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ src/programmers/lv.1/lesson_12954.js
@@ -0,0 +1,14 @@
+// x만큼 간격이 있는 n개의 숫자
+// https://school.programmers.co.kr/learn/courses/30/lessons/12954
+
+function solution(x, n) {
+  let answer = []
+
+  for (i = 1;  i <= n; i++) {
+    answer.push(x * i)
+  }
+
+  return answer
+}
+
+console.log(solution(2, 5))
