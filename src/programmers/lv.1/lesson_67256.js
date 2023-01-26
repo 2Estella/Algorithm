1118d55cb0e3b41b982e8ff46e92e753adfcaf39
2Estella <96452250+2Estella@users.noreply.github.com>
2023-01-26 13:25:37 +0900

    키패드 누르기

---
+++ src/programmers/lv.1/lesson_67256.js
@@ -0,0 +1,17 @@
+// 키패드 누르기
+// https://school.programmers.co.kr/learn/courses/30/lessons/67256?language=javascript
+
+function solution(numbers, hand) {
+  let answer = ''
+
+  const keypad = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    ['*', 0, '#']
+  ]
+
+  return answer
+}
+
+console.log(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], 'right'))
